027a9d8d32c597958d6305649f4954f5a76c5b09
João Victor Amaral <123979797+joaoamaral-dev@users.noreply.github.com>
2024-01-20 21:14:19 -0300

    Add files via upload

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,26 @@
+alert("Seja bem vindo ao jogo do número secreto!");
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+  chute = prompt(`Escolha um número entre 0 e ${numeroMaximo}!`);
+  if (numeroSecreto == chute) {
+    break;
+  } else {
+    if (chute > numeroSecreto) {
+      alert(`O número secreto é menor que ${chute}`);
+    } else {
+      alert(`O número secreto é maior que ${chute}`);
+    }
+    tentativas++;
+  }
+}
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+
+alert(
+  `Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`
+);
